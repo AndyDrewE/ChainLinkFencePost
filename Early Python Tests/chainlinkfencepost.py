5b1ca5e6c6fec6912cbfd461bc55ba5885f17897
AndyDrewE <easleyandrewp@gmail.com>
2025-07-30 15:53:50 -0500

    find available letters and pick a random letter
    Can now find available letters from the existing word pairs list and pick a random one

--- Early Python Tests/chainlinkfencepost.py
+++ Early Python Tests/chainlinkfencepost.py
@@ -1,7 +1,22 @@
+import random as rnd
+
 word_pairs = ["water slide", "slide show", "show girl", "girl scout", "scout leader", "slide deck", "show stopper", "water softener", "leader board", "apple pie", "pie crust"]
 
-def extract_first_words(pairs):
+def find_available_letters(pairs):
+    letters_available = []
     for pair in pairs:
-        print(pair.split(" ")[0][0])
+        first_letter = pair.split(" ")[0][0]
+        if first_letter not in letters_available:
+            letters_available.append(first_letter)
+    
+    return letters_available
 
-extract_first_words(word_pairs)
+def pick_random_letter(letters):
+    num_letters = len(letters)
+    rand_index = rnd.randint(0, num_letters - 1)
+    return letters[rand_index]
+
+def main():
+    current_letters = find_available_letters(word_pairs)
+    rand_letter = pick_random_letter(current_letters)
+    
