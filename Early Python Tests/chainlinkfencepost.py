21c44518adf68690fc70d6316d709ed99402108a
AndyDrewE <easleyandrewp@gmail.com>
2025-07-30 14:30:43 -0500

    Update python tester

--- Early Python Tests/chainlinkfencepost.py
+++ Early Python Tests/chainlinkfencepost.py
@@ -1,4 +1,7 @@
-word_pairs = ["water slide", "slide show", "show girl", "girl scout", "scout leader", "slide deck", "show stopper", "water softener"]
+word_pairs = ["water slide", "slide show", "show girl", "girl scout", "scout leader", "slide deck", "show stopper", "water softener", "leader board", "apple pie", "pie crust"]
 
+def extract_first_words(pairs):
+    for pair in pairs:
+        print(pair.split(" ")[0][0])
 
-print(word_pairs)
+extract_first_words(word_pairs)
