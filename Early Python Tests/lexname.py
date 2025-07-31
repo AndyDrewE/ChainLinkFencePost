60daa3dab9ce1b9c120381d5685d09e77b428db0
AndyDrewE <easleyandrewp@gmail.com>
2025-07-31 13:56:33 -0500

    playing with word net
    I think I might have to make a bespoke solution for a word set

---
+++ Early Python Tests/lexname.py
@@ -0,0 +1,4 @@
+from nltk.corpus import wordnet as wn
+
+#distinct_lexnames = sorted({syn.lexname() for syn in wn.all_synsets()})
+
--- Early Python Tests/twowordphrases.py
+++ Early Python Tests/twowordphrases.py
@@ -7,6 +7,7 @@ import time
 
 phrases = set()
 
+
 t0_total = time.perf_counter()
 
 #Get distinct names in all of the synsets to process
@@ -47,35 +48,6 @@ print(f"Number of Phrases: {len(phrases)}")
 t1_total = time.perf_counter()
 print(f"Finished in {t1_total - t0_total:.2f} seconds")
 
-
-''' Pre Optimized Code
-#All synsets in Wordnet
-for synset in wn.all_synsets():
-    for lemma in synset.lemmas():
-        #Each synset has associated words or multiword terms
-        name = lemma.name()
-        parts = name.split('_')
-
-        #must be 2 words
-        if len(parts) != 2:
-            continue
-
-        #Skip non-alphabetic words (i.e. anything with punctuation, numbers, and anything less than one letter)
-        if not all(valid_word.match(part) for part in parts):
-            continue
-
-        #Skip if proper noun, 'NNP' Proper noun singular, 'NNPS' proper noun plural
-        tags = pos_tag(parts)
-        if any(tag in ('NNP', 'NNPS') for _, tag in tags):
-            continue
-        
-        phrase = ' '.join(parts).lower()
-        #watch yo profanity,
-        #kind of heavy handed profanity checker, but it works
-        if not profanity.contains_profanity(phrase):
-            phrases.add(phrase)
-        
-'''
 
 output_file = "two_word_phrases.txt"
 with open(output_file, "w", encoding="utf-8") as f:
