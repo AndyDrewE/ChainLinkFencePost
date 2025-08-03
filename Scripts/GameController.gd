15f5c3e8d967abab5f900471f8250d3fa1ff0a44
AndyDrewE <easleyandrewp@gmail.com>
2025-08-03 15:50:40 -0500

    Update GameController.gd

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -1,7 +1,7 @@
 ## Game Controller.gd
 extends Node
 
-var word_pairs = ["bottled water", "water slide", "slide show", "show girl", "girl scout", "scout leader", "leader board", "board game", "game over", "game on", "over easy", "easy bake", "easy living", "bake off", "bake well", "well done", "done for", "off side", "in side"]
+var word_pairs = ["bottled water", "water slide", "slide show", "show girl", "girl scout", "scout leader", "leader board", "board game", "game over", "game on", "over easy", "easy bake", "easy living", "bake off", "bake well", "well done", "done for", "off side", "in side", "water softener"]
 
 
 
