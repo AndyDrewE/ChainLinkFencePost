4f5e23c299fe923b99f4ae79576e0102002b2097
AndyDrewE <easleyandrewp@gmail.com>
2025-08-03 15:24:32 -0500

    Game controller, first word pairs

---
+++ Scripts/GameController.gd
@@ -0,0 +1,8 @@
+## Game Controller.gd
+extends Node
+
+var word_pairs = ["bottled water", "water slide", "slide show", "show girl", "girl scout", "scout leader", "leader board", "board game", "game over", "game on", "over easy", "easy bake", "easy living", "bake off", "bake well", "well done", "done for", "off side", "in side"]
+
+
+
+
