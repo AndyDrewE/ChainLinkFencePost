49b9f0d01574609711721aced642141c2652e902
AndyDrewE <easleyandrewp@gmail.com>
2025-08-03 21:37:51 -0500

    Ported over some code from early tests

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -2,7 +2,26 @@
 extends Node
 
 var word_pairs = ["bottled water", "water slide", "slide show", "show girl", "girl scout", "scout leader", "leader board", "board game", "game over", "game on", "over easy", "easy bake", "easy living", "bake off", "bake well", "well done", "done for", "off side", "in side", "water softener"]
+var word_pairs_dict = {}
 
+func _ready():
+	# Key the word dictionary
+	key_word_dict()
 
+func key_word_dict():
+	for pair in word_pairs:
+		var pair_parts = pair.split(" ")
+		var key = pair_parts[0]
+		if not word_pairs_dict.has(key):
+			word_pairs_dict[key] = []
+		word_pairs_dict[key].append(pair)
 
+func get_usable_pairs(word):
+	return word_pairs_dict.get(word, [])
 
+func is_valid(current_word, previous_word=""):
+	if previous_word == "":
+		return current_word in word_pairs_dict.keys()
+	
+	var valid = (previous_word + current_word) in word_pairs
+	return valid
--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -4,16 +4,29 @@ extends Control
 @onready var player_input_textbox = $PlayerInput
 @onready var previous_words = $PreviousWordsBox
 
+var previous_word = ""
+
+
 
 func create_label(label_text):
 	var new_label = Label.new()
 	new_label.text = label_text
 	new_label.add_theme_font_size_override("font_size", 48)
 	previous_words.add_child(new_label)
+	previous_word = label_text
+
+func flash_label_invalid():
+	var original_color = player_input_textbox.modulate
+	player_input_textbox.modulate = Color.RED
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(player_input_textbox, "modulate", original_color, 0.5) # 0.5 sec
 
 
 func _on_player_input_text_submitted(new_text):
-	create_label(new_text)
-	
-	#Erase Text
-	player_input_textbox.text = ""
+	if GameController.is_valid(new_text, previous_word):
+		create_label(new_text)
+		#Erase Text
+		player_input_textbox.text = ""
+	else:
+		flash_label_invalid()
