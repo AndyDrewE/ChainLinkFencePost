8eaf08cf1641b0d2af92c86039b757c39f530604
AndyDrewE <easleyandrewp@gmail.com>
2025-08-04 21:45:29 -0500

    Added Lives and Score
    Added lives, score, a game over state. TODO: Expand list of word pairs, add a pause and menu screen, add a restart, expand game loop

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -4,9 +4,15 @@ extends Node
 var word_pairs = ["bottled water", "water slide", "slide show", "show girl", "girl scout", "scout leader", "leader board", "board game", "game over", "game on", "over easy", "easy bake", "easy living", "bake off", "bake well", "well done", "done for", "off side", "in side", "water softener"]
 var word_pairs_dict = {}
 
+var player_lives = 3
+var player_score = 0
+
 func _ready():
 	# Key the word dictionary
 	key_word_dict()
+
+func get_UI_controller() -> Node:
+	return get_tree().current_scene.get_node("UIController")
 
 func key_word_dict():
 	for pair in word_pairs:
@@ -20,8 +26,23 @@ func get_usable_pairs(word):
 	return word_pairs_dict.get(word, [])
 
 func is_valid(current_word, previous_word=""):
+	#print("Previous Word: " + previous_word)
+	#print("Current Word: " + current_word)
+	
 	if previous_word == "":
 		return current_word in word_pairs_dict.keys()
 	
-	var valid = (previous_word + current_word) in word_pairs
+	var valid = (previous_word + " " + current_word) in word_pairs
+	#print(valid)
 	return valid
+
+func update_lives(cost):
+	player_lives += cost
+	get_UI_controller().update_lives_label()
+	
+	if player_lives == 0:
+		get_UI_controller().game_over()
+
+func update_score(cost):
+	player_score += cost
+	get_UI_controller().update_score_label()
--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -1,19 +1,38 @@
 extends Control
 
-
+#Player Input Variables
 @onready var player_input_textbox = $PlayerInput
 @onready var previous_words = $PreviousWordsBox
 
+#Stats Box variables
+@onready var num_lives_label = $StatsBox/Lives/NumLives
+@onready var score_label = $StatsBox/Score/ScoreLabel
+
+#GameOver
+@onready var game_over_label = $GameOver
+
 var previous_word = ""
 
+func _ready():
+	update_stats_box()
 
+## Update all in the stats box
+func update_stats_box():
+	update_lives_label()
+	update_score_label()
 
+func update_lives_label():
+	num_lives_label.text = "%d" % GameController.player_lives
+
+func update_score_label():
+	score_label.text = "%d" % GameController.player_score
+
+#Creates label for previous words to display
 func create_label(label_text):
 	var new_label = Label.new()
 	new_label.text = label_text
 	new_label.add_theme_font_size_override("font_size", 48)
 	previous_words.add_child(new_label)
-	previous_word = label_text
 
 func flash_label_invalid():
 	var original_color = player_input_textbox.modulate
@@ -22,11 +41,22 @@ func flash_label_invalid():
 	var tween = get_tree().create_tween()
 	tween.tween_property(player_input_textbox, "modulate", original_color, 0.5) # 0.5 sec
 
+func game_over():
+	game_over_label.visible = true
+	player_input_textbox.editable = false
 
 func _on_player_input_text_submitted(new_text):
-	if GameController.is_valid(new_text, previous_word):
-		create_label(new_text)
-		#Erase Text
-		player_input_textbox.text = ""
-	else:
-		flash_label_invalid()
+	if player_input_textbox.editable:
+		new_text = new_text.to_lower()
+		if GameController.is_valid(new_text, previous_word):
+			create_label(new_text)
+			previous_word = new_text
+			#Erase Text
+			player_input_textbox.text = ""
+			
+			#Add however long the word is to score
+			GameController.update_score(len(new_text))
+		else:
+			flash_label_invalid()
+			#Take away a life
+			GameController.update_lives(-1)
